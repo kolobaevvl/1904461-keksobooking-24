450c7791cab7bfccd2a4a77119cd3fbd477ea6e7
kolal03@inbox.ru <kolal03@inbox.ru>
2021-10-15 14:19:08 +0300

    генерация объявлений

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-function getRandInteger(from, to) {
+
+const getRandInteger = (from, to) => {
   if (from < 0 || to <= from) {
     throw new RangeError('Числа не подходят.');
   }
   return Math.round(from - 0.5 + Math.random() * (to - from + 1));
-}
+};
 try {
   getRandInteger(100, 300);
 }
@@ -14,12 +15,12 @@ catch(error)
   }
 }
 
-function getRandNumber(from, to, precision) {
+const getRandNumber = (from, to, precision) => {
   if (from < 0 || to <= from) {
     throw new RangeError('Числа не подходят.');
   }
   return Number((Math.random() * (to - from) + from).toFixed(precision));
-}
+};
 try {
   getRandNumber(1.5, 2.9, 4);
 }
@@ -30,89 +31,90 @@ catch(error)
   }
 }
 
+const ADVERT_COUNT = 10;
+const ADVERT_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+const CHECKIN_TIME = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const CHECKOUT_TIME = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES_ADVERT = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const PHOTOS_ADVERT = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+const RANDOM_ADVERTS = [];
 
-const finalArr = [];
-const countObjects = 10;
-const typesArray = [
-	'palace', 
-	'flat', 
-	'house', 
-	'bungalow', 
-	'hotel'
-];
-const checkinTime = [
-	'12:00', 
-	'13:00', 
-	'14:00'
-];
-const checkoutTime = [
-	'12:00', 
-	'13:00', 
-	'14:00'
-];
-const featuresArray = [
-	'wifi', 
-	'dishwasher', 
-	'parking', 
-  'washer', 
-	'elevator', 
-	'conditioner',
-];
-const photosArray = [
-	'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-	'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-	'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
-];
-
-function getAuthorAvatar(id) {
-  const userAvatarID = String(++id).padStart(2,0);
+const getAuthorAvatar = (id) => {
+  const userAvatarID = String(id).padStart(2,0);
   return `img/avatars/user${userAvatarID}.png`;
-}
+};
 
-function getArray(randomLength) {
+const getRandomItemsInArray = (randomLength) => {
   const maxLength = randomLength.length;
   const lengthOfArray = getRandInteger(1, maxLength);
-  const array = [];
-  for(let foo = 0; foo < lengthOfArray; foo++) {
-    const indexOfEl = getRandInteger(0, maxLength - 1);
-    const el = randomLength[indexOfEl];
-    if (!array.includes(el)) {
-      array.push(el);
+  const items = [];
+  for(let i = 0; i < lengthOfArray; i++) {
+    const randomIndex = getRandInteger(0, maxLength - 1);
+    const randomElement = randomLength[randomIndex];
+    if (!items.includes(randomElement)) {
+      items.push(randomElement);
     }
   }
-  return array;
-}
+  return items;
+};
 
-function getObjects () {
-
-  for(let i = 0; i < countObjects; i++) {
+const getRandomAdverts = (ADVERT_COUNT2) => {
+  for(let j = 0; j < ADVERT_COUNT2; j++) {
     const lat = getRandNumber(35.65000, 35.70000, 5);
     const lng = getRandNumber(139.70000, 139.80000, 5);
 
-    const offerObject = {
+    const adverts = {
       author: {
-        avatar: getAuthorAvatar(i),
+        avatar: getAuthorAvatar(j+1),
       },
       offer: {
         title: 'Название объявления',
-        adress: `${lat} ${lng}`,
+        address: `${lat}, ${lng}`,
         price: getRandInteger(1, 1000),
-        type: typesArray[getRandInteger(0, 4)],
+        type: ADVERT_TYPES[getRandInteger(0, ADVERT_TYPES.length - 1)],
         rooms: getRandInteger(1, 5),
         guests: getRandInteger(1, 10),
-        checkin: checkinTime[getRandInteger(0, 2)],
-        checkout: checkoutTime[getRandInteger(0, 2)],
-        features: getArray(featuresArray),
+        checkin: CHECKIN_TIME[getRandInteger(0, 2)],
+        checkout: CHECKOUT_TIME[getRandInteger(0, 2)],
+        features: getRandomItemsInArray(FEATURES_ADVERT),
         description: 'Описание объекта объявления',
-        photos: getArray(photosArray),
+        photos: getRandomItemsInArray(PHOTOS_ADVERT),
       },
-			location: {
-				lat: lat,
-        lng: lng,
-			}
+      location: {
+        lat,
+        lng,
+      },
     };
-    finalArr.push(offerObject);
+    RANDOM_ADVERTS.push(adverts);
   }
-}
+  return RANDOM_ADVERTS;
+};
 
-getObjects();
+getRandomAdverts(ADVERT_COUNT);
+
+// const FINAL_ADVERTS = getRandomAdverts(ADVERT_COUNT);
